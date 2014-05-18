9fa31d9ac6a7608eeb8b72628f986d9b30610624
solomonsuheng <1185626206@qq.com>
2014-05-18 13:43:44 +0800

    MongoDB测试连接获取数据

--- Connect2DB/MongoDB/MongoDB.java
+++ Connect2DB/MongoDB/MongoDB.java
@@ -7,20 +7,18 @@ import com.mongodb.Mongo;
 
 /**
  * @ClassName: MongoDB
- * @Description: Test connecte to mongoDB and get info from mongoDB DB:news
- *               Collections:user
+ * @Description: TODO连接MongoDB数据库,news数据库的user集合
  * @author solomonsuheng
- * @date 2014-5-18 11:06:13am
+ * @date 2014-5-18 下午1:41:21
  */
 public class MongoDB {
 	public static void main(String[] args) {
-		Mongo mongo;
 		try {
-			mongo = new Mongo();
-			DB db = mongo.getDB("news");
-			DBCollection dbc = db.getCollection("user");
-			DBCursor cur = dbc.find();
-			while (cur.hasNext()) {
+			Mongo mongo = new Mongo();// 默认是localhost：27017
+			DB db = mongo.getDB("news");// 从MongoDB连接中获取news数据库
+			DBCollection dbc = db.getCollection("user");// 从news数据库获取user集合
+			DBCursor cur = dbc.find();// 从user集合中查询所有数据,返回给游标cur
+			while (cur.hasNext()) {// 根据游标cur的值输出数据
 				System.out.println(cur.next().toString());
 			}
 		} catch (UnknownHostException e) {
